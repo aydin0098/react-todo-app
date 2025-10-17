be573bc6d2cc16e45fd339f9da54ef8327579224
aydin0098 <aydin.s.hagighy@gmail.com>
2025-10-18 01:49:41 +0330

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-todo-app/', // ⬅️ این مهم‌ترین خطه!
+  base: '/react-todo-app/'
 })
